4097b54e3827fba77efac387ed140fb2aa8f8eb1
himanshu0089 <88453380+himanshu0089@users.noreply.github.com>
2024-09-04 22:28:33 +0530

    Refractor: Container

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Container.css'
 
 function Container() {
   return (
     <div className='container'>
-      <div>
+      <div className='tech'>
         <h3>Technologies</h3>
         <p>I'm familiar with HTML5, CSS3, Git, JavaScript, NodeJS, ReactJS, and Web Hosting</p>
       </div>
-      <div>
+      <div className='project'>
         <h3>Projects</h3>
         <p>I like to showcase my work and thus, you can see my projects hosted online</p>
         <Link to='/project'><button>See Projects</button></Link>
       </div>
-      <div>
+      <div className='container-blog'>
         <h3>Blogs</h3>
         <p>I am also working on some technical and non techincal blogs. I like to document my journey of learning.</p>
-        <Link to='/blog'>Read Blogs</Link>
+        <Link to='/blog' className='container-blog-link'>Read Blogs</Link>
       </div>
       </div>
   )
