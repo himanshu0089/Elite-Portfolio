7b7ba76134f01cbeaac302d68bad71617e86a14a
himanshu0089 <88453380+himanshu0089@users.noreply.github.com>
2024-09-04 22:31:51 +0530

    Refractor:App

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,29 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Container from "./components/Container/Container";
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route, Link } from "react-router-dom";
-import Project from "./Pages/Project";
-import Blog from "./Pages/Blog";
+import Project from "./Pages/Projects/Project";
+import Blog from "./Pages/Blogs/Blog";
+import Home from "./components/Home/Home";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
 
 function App() {
   return (
     <>
       <div>
         <Header />
-
+         
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Container />
-                <Footer />
-              </>
-            }
-          />
+          <Route path="/" element={<Home />} />
           <Route path="/project" element={<Project />} />
-          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog" element={<Blog />} />  
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
-       
+           
+        <Footer />
       </div>
     </>
   );
