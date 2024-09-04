e1759d699755fc2f01bd1949707cad9ab4f06355
himanshu0089 <88453380+himanshu0089@users.noreply.github.com>
2024-09-04 22:34:08 +0530

    Refractor & Add:Home

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Banner.css"
+
+function Banner() {
+  return (
+    <div className='banner'>
+      <div>
+        {/* <img src="port.jpg" alt="" />  */}
+      <h2>I love to design the sites</h2>
+        <p>
+          To view the 
+        </p>
+        <button>view menu</button>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Home.css";
+import Container from '../Container/Container';
+import Banner from '../Banner/Banner';
+
+function Home() {
+  return (
+    <div>
+        <Banner />
+        <Container />
+    </div>
+  )
+}
+
+export default Home
