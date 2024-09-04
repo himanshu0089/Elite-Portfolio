bd69db0d8942d1d29d0ab516681fec6ce9e91b0c
himanshu0089 <88453380+himanshu0089@users.noreply.github.com>
2024-09-04 22:25:43 +0530

    Refractor: Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import './Footer.css';
 import { Link } from "react-router-dom";
+import { assets } from "../../assets/assets";
 function Footer() {
   return (
-    <div>
-      <footer>
-      <div>
+    <div className="footer">
+      <div className="title">
         <h2>Connect me through</h2>
       </div>
-      <div>
-        <Link className="ref">Github</Link>
-        <Link className="ref">Linkedin</Link>
+      <div className="footer-links">
+        <Link to="https://github.com/himanshu0089" className="refs"><img src={assets.twitter_icon} alt="" /></Link>
+        <Link to="https://www.linkedin.com/in/himanshu0089/" className="refs"><img src={assets.linkedin_icon} alt="" /></Link>
+        <Link to="https://github.com/himanshu0089" className="refs"><img src={assets.facebook_icon} alt="" /></Link>
       </div>
 
       <div className="signature">
         {" "}
         <h3>Designed by Pir@te</h3>
       </div>
-      </footer>
     </div>
   );
 }
