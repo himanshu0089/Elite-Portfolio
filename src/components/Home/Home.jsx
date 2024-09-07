025610774df671e21a3160b844fee89e0f216193
himanshu0089 <88453380+himanshu0089@users.noreply.github.com>
2024-09-07 23:29:42 +0530

    Refractor:Home code

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import "./Home.css";
 import Container from '../Container/Container';
 import Banner from '../Banner/Banner';
+import About from '../../Pages/About/About';
 
 function Home() {
   return (
     <div>
         <Banner />
+        <About />
         <Container />
     </div>
   )
