233041b35c3377e2f1b7e5bae3d21f38fcbbe960
himanshu0089 <88453380+himanshu0089@users.noreply.github.com>
2024-09-08 23:46:39 +0530

    Setup: Render Projects

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,12 +3,14 @@ import "./Home.css";
 import Container from '../Container/Container';
 import Banner from '../Banner/Banner';
 import About from '../../Pages/About/About';
+import Project from '../../Pages/Projects/Project';
 
 function Home() {
   return (
     <div>
         <Banner />
         <About />
+        <Project />
         <Container />
     </div>
   )
