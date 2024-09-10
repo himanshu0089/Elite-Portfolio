97e64ad757689fcc24176f472f7fe3ef65bc8d23
himanshu0089 <88453380+himanshu0089@users.noreply.github.com>
2024-09-10 18:48:38 +0530

    Refractor components

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -9,11 +9,7 @@ function Container() {
         <h3>Technologies</h3>
         <p>I'm familiar with HTML5, CSS3, Git, JavaScript, NodeJS, ReactJS, and Web Hosting</p>
       </div>
-      <div className='project'>
-        <h3>Projects</h3>
-        <p>I like to showcase my work and thus, you can see my projects hosted online</p>
-        <Link to='/project'><button>See Projects</button></Link>
-      </div>
+      
       <div className='container-blog'>
         <h3>Blogs</h3>
         <p>I am also working on some technical and non techincal blogs. I like to document my journey of learning.</p>
